6c2cfe3595fcd9090dd149b95af3d1bee3558820
Kristiaan <kszoontjens@gmail.com>
2018-01-18 11:09:34 +0100

    IT is working, can get and post after one anohter

--- Vehicle.py
+++ Vehicle.py
@@ -4,20 +4,17 @@ import urllib
 
 
 def Vehicle():
+    date = raw_input("What date would you like to park (YY-MM-DD HH:MM)")
+    duration = raw_input("How long would you like to park (minutes)")
+    GETRequest(date, duration);
     name = raw_input("What parkingspot would you like to take? ")
-    date = raw_input("What date would you like to park (YY-MM-DD HH:MM)")
-    duration = raw_input("How long would you like to park (minutes)")
-    print("Making reservation for parkingspot " + str(name) + ", At date " + str(date) + " for " + str(duration) + " minutes!")
     licensePlate = raw_input("What is your cars license plate?")
-
     POSTRequest(name, date, duration, licensePlate)
+    print("Making reservation for parkingspot " + str(name) + ", At date " + str(date) + " for " + str(duration) + " minutes!")
 
-
-def GETRequest():
+def GETRequest(date, duration):
     import httplib
-    conn = httplib.HTTPConnection("192.168.1.13", "8085")
-    date = raw_input("What date would you like to park (YY-MM-DD HH:MM)")
-    duration = raw_input("How long would you like to park (minutes)")
+    conn = httplib.HTTPConnection("131.155.239.0", "8085")
     #params = urllib.urlencode(dict({'starttime': date, 'duration': raw_input}))
     #url = "http://example.com/?a=text&q2=text2&q3=text3&q2=text4"
     f = {'duration': duration, 'starttime': date}
@@ -30,7 +27,7 @@ def POSTRequest(name, date, duration, licensePlate):
 
     params = urllib.urlencode({'licensePlate': str(licensePlate), 'type': 'auto', 'parkingspot': name, 'duration': str(duration), 'starttime': str(date)})
     headers = {"Content-type": "application/x-www-form-urlencoded"}
-    conn = httplib.HTTPConnection("192.168.1.13", "8085")
+    conn = httplib.HTTPConnection("131.155.239.0", "8085")
     conn.request("POST", "", params, headers)
     response = conn.getresponse()
     print("reservation for parkingspot " + str(name) + ", At date " + str(date) + " for " + str(duration) + " minutes succesfull!");
@@ -38,5 +35,5 @@ def POSTRequest(name, date, duration, licensePlate):
     conn.close()
 
 
-GETRequest();
-#Vehicle();
+
+Vehicle();
